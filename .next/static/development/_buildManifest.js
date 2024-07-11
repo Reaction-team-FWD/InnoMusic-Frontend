1f9d1225072be970e8e6920ec00036204a41aa9e
Maxim Fomin <fominmaxim3721@gmail.com>
2024-07-11 12:14:30 +0300

    chore: remove .next from git

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/likedsongs":["static\u002Fchunks\u002Fpages\u002Flikedsongs.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Flikedsongs"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
