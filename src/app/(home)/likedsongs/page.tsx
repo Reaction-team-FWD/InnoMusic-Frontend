896c1d53eaca606c43ea04f6f31f02c993435c16
netimaaa <t.farizunov@innopolis.university>
2024-07-20 01:55:12 +0300

    icons + seo

--- src/app/(home)/likedsongs/page.tsx
+++ src/app/(home)/likedsongs/page.tsx
@@ -6,8 +6,25 @@ const SongsPage: React.FC = () => {
   return (
     <>
       <Head>
-        <title>Liked Songs</title>
-        <meta name="description" content="" />
+        <title>Liked Songs - Your Favorite Music</title>
+        <meta
+          name="description"
+          content="View and manage your favorite songs. Discover and listen to all the music you love in one place."
+        />
+        <meta
+          name="keywords"
+          content="liked songs, favorite music, music playlist, favorite tracks"
+        />
+        <meta property="og:title" content="Liked Songs - Your Favorite Music" />
+        <meta
+          property="og:description"
+          content="View and manage your favorite songs. Discover and listen to all the music you love in one place."
+        />
+        <meta property="og:image" content="/img/likedSongsCover.png" />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
       </Head>
       <Songs />;
     </>
