913fbed4b895b3c6185964189aefcedfa791fa2d
Maxim Fomin <fominmaxim3721@gmail.com>
2024-07-20 00:52:14 +0300

    refactor: style

--- src/components/Songs/Songs.tsx
+++ src/components/Songs/Songs.tsx
@@ -1,39 +1,30 @@
 // src/pages/songs.tsx
-import {SongsView} from "@/components/Songs/SongsView";
+import { SongsView } from '@/components/Songs/SongsView';
 
 import React from 'react';
-import Song from '@/components/Song/Song';
-import styles from './Songs.module.scss';
-import {satisfies} from "next/dist/lib/semver-noop";
-import {GetServerSideProps, InferGetServerSidePropsType} from "next";
+import Song from '@/entities/song/Song';
 
-interface SongData {
-  id: string;
-  name: string;
-  artist: string;
-  duration: string;
-}
-
-export const getServerSideProps = (async () =>{
-  let initialSongs: SongData[] = [];
+export const getServerSideProps = (async () => {
+  let initialSongs: Song[] = [];
   try {
-    const response = await fetch("https://b7ff-188-130-155-162.ngrok-free.app/song/all");
+    const response = await fetch('https://b7ff-188-130-155-162.ngrok-free.app/song/all');
     initialSongs = await response.json();
   } catch (error) {
     console.error('Failed to fetch songs:', error);
   }
 
   return initialSongs;
-  
-}) 
+
+});
 
 export async function Songs() {
   const initialSongs = await getServerSideProps();
-  return(
-      <>
-        <SongsView initialSongs={initialSongs} />
-      </>
-  )
-  
+  return (
+    <>
+      <SongsView initialSongs={initialSongs} />
+    </>
+  );
+
 }
+
 export default Songs;
