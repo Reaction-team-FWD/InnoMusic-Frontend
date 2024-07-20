13c9a736c426cb348553404a3331b8e9a33c4afb
Maxim Fomin <fominmaxim3721@gmail.com>
2024-07-20 07:07:18 +0300

    fix: about page styles

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -1,13 +1,11 @@
 import styles from './about.module.scss';
-import './about.scss';
 import Image from 'next/image';
 
 export const metadata = {
   title: 'About Us',
   description: 'Learn more about Spotify',
 };
-import './about.module.scss';
-import './about.scss';
+
 import Oleg from '/public/img/photos/oleg.jpg';
 import Max from '/public/img/photos/max.jpg';
 import Egor from '/public/img/photos/egor.jpg';
@@ -16,62 +14,74 @@ import Tim from '/public/img/photos/tim.jpg';
 
 export default function AboutPage() {
   return (
-    <div className="about-container">
-      <main className="about-main">
-        <h1>About Us</h1>
-        <section>
-          <h2>Our Mission</h2>
-          <p>
-            Our mission is to unlock the potential of human creativity—by giving a million creative artists the
-            opportunity to live off their art and billions of fans the opportunity to enjoy and be inspired by it.
-          </p>
-        </section>
+    <div className={styles.background}>
+      <div className={styles.about_container}>
+        <main className={styles.about_main}>
+          <h1 className={styles.heading_max}>About Us</h1>
+          <section className={styles.section}>
+            <h2 className={styles.heading_big}>Our Mission</h2>
+            <p className={styles.para}>
+              Our mission is to unlock the potential of human creativity — by giving a million creative artists the
+              opportunity to live off their art and billions of fans the opportunity to enjoy and be inspired by it.
+            </p>
+          </section>
 
-        <section>
-          <h2>Our Team</h2>
-          <div className="team-member">
-            <h3>Oleg Shchendrigin</h3>
-            <Image src={Oleg} width={500} height={500} alt="Oleg Shchendrigin" className="team-photo" />
-          </div>
-          <div className="team-member">
-            <h3>Maxim Fomin</h3>
-            <Image src={Max} width={500} height={500} alt="Maxim Fomin" className="team-photo" />
-          </div>
-          <div className="team-member">
-            <h3>Egor Chernobrovkin</h3>
-            <Image src={Egor} width={500} height={500} alt="Egor Chernobrovkin" className="team-photo" />
-          </div>
-          <div className="team-member">
-            <h3>Adilia Saifetdiarova</h3>
-            <Image src={DiDi} width={500} height={500} alt="Adilia Saifetdiarova" className="team-photo" />
-          </div>
-          <div className="team-member">
-            <h3>Timur Farizunov</h3>
-            <Image src={Tim} width={500} height={500} alt="Timur Farizunov" className="team-photo" />
-          </div>
-        </section>
+          <section className={styles.section}>
+            <h2 className={styles.heading_big}>Our Team</h2>
+            <div className={styles.team_member}>
+              <h3>Oleg Shchendrigin</h3>
+              <Image src={Oleg} width={500} height={500} alt="Oleg Shchendrigin" className={styles.team_photo} />
+            </div>
+            <div className={styles.team_member}>
+              <h3>Maxim Fomin</h3>
+              <Image src={Max} width={500} height={500} alt="Maxim Fomin" className={styles.team_photo} />
+            </div>
+            <div className={styles.team_member}>
+              <h3>Egor Chernobrovkin</h3>
+              <Image src={Egor} width={500} height={500} alt="Egor Chernobrovkin" className={styles.team_photo} />
+            </div>
+            <div className={styles.team_member}>
+              <h3>Adilia Saifetdiarova</h3>
+              <Image src={DiDi} width={500} height={500} alt="Adilia Saifetdiarova" className={styles.team_photo} />
+            </div>
+            <div className={styles.team_member}>
+              <h3>Timur Farizunov</h3>
+              <Image src={Tim} width={500} height={500} alt="Timur Farizunov" className={styles.team_photo} />
+            </div>
+          </section>
 
-        <section>
-          <h2>Contact Us</h2>
-          <ul className="contact-list">
-            <li>
-              <a href="mailto:o.shchendrigin@innopolis.university">o.shchendrigin@innopolis.university</a>
-            </li>
-            <li>
-              <a href="mailto:m.fomin@innopolis.university">m.fomin@innopolis.university</a>
-            </li>
-            <li>
-              <a href="mailto:e.chernobrovkin@innopolis.university">e.chernobrovkin@innopolis.university</a>
-            </li>
-            <li>
-              <a href="mailto:sayfetik2005@gmail.com">sayfetik2005@gmail.com</a>
-            </li>
-            <li>
-              <a href="mailto:t.farizunov@innopolis.university">t.farizunov@innopolis.university</a>
-            </li>
-          </ul>
-        </section>
-      </main>
+          <section className={styles.section}>
+            <h2 className={styles.heading_big}>Contact Us</h2>
+            <ul className={styles.contact_list}>
+              <li>
+                <a className={styles.email} href="mailto:o.shchendrigin@innopolis.university">
+                  o.shchendrigin@innopolis.university
+                </a>
+              </li>
+              <li>
+                <a className={styles.email} href="mailto:m.fomin@innopolis.university">
+                  m.fomin@innopolis.university
+                </a>
+              </li>
+              <li>
+                <a className={styles.email} href="mailto:e.chernobrovkin@innopolis.university">
+                  e.chernobrovkin@innopolis.university
+                </a>
+              </li>
+              <li>
+                <a className={styles.email} href="mailto:sayfetik2005@gmail.com">
+                  sayfetik2005@gmail.com
+                </a>
+              </li>
+              <li>
+                <a className={styles.email} href="mailto:t.farizunov@innopolis.university">
+                  t.farizunov@innopolis.university
+                </a>
+              </li>
+            </ul>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
